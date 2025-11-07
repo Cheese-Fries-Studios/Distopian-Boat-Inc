469189e93a3b0f6ee5f2c908a39322f4c67aea8c
PolyCat <contact.polycat@proton.me>
2025-11-07 16:29:06 +0200

    made boat scene

---
+++ navigation/boat.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+const SPEED := 200
+
+var target = null
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		target = get_global_mouse_position()
+
+
+
+func _physics_process(delta):
+	if target:
+		var dir = target - global_position
+		if global_position.distance_to(target) > 1:
+			rotation = dir.angle() + deg_to_rad(-90)
+			global_position = global_position.move_toward(target, SPEED * delta)
+		else:
+			target = null
+
+
