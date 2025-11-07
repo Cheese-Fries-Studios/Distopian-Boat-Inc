e2fd2087138971b17a01b3c53a07bf9b7fc62575
Lambdand <lambdan@proton.me>
2025-11-07 12:40:09 +0200

    player movement

--- player/player.gd
+++ player/player.gd
@@ -1,4 +1,12 @@
 extends CharacterBody2D
 
-func _physics_process(_delta: float) -> void:
-	pass
+@export var speed = 400
+
+func get_input():
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	print(input_dir)
+	velocity = input_dir * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
