f69bf48e314ad9c5549445f54dad8731a25a22dd
Lambdand <lambdan@proton.me>
2025-11-07 15:10:05 +0200

    make script work gut

--- player/player.gd
+++ player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_dir = Input.get_vector("right", "left", "down", "up")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	print(input_dir)
 	velocity = input_dir * speed
 
