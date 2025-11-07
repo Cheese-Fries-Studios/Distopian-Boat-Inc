06b4a18f4fb3b4662adb356e03cc6a8ceb3e12db
Lambdand <lambdan@proton.me>
2025-11-07 14:43:54 +0200

    added build mode UI toggle

---
+++ control.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _process(delta: float) -> void:
+	var input = Input.is_action_just_pressed("build_mode")
+	is_visible()
+	if input == true:
+		if visible == true:
+			set_visible(false)
+		else:
+			set_visible(true)
