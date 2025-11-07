7a00b7c50d1a73e940fcd6bf0bf66f36d808ed9b
PolyCat <contact.polycat@proton.me>
2025-11-08 01:04:38 +0200

    made trash piles dissapear once collected

--- navigation/trash_pile.gd
+++ navigation/trash_pile.gd
@@ -7,9 +7,17 @@ var random_scale: int = 0
 func _ready() -> void:
 	area_entered.connect(emit_size) 
 
+	$Timer.wait_time = 0.3
+	$Timer.timeout.connect(timeout)
+
 	random_scale = 6 + randi() % 13
 	scale = Vector2(random_scale, random_scale)
 	
 
 func emit_size(_x):
 	boat_entered.emit(random_scale)
+	$Timer.start()
+
+
+func timeout():
+	call_deferred("free")
